11c639198600a9744d1d1ad64ee0314aad33c092
Tushar Chauhan <166517064+tusharchauahan@users.noreply.github.com>
2024-04-10 11:23:45 +0530

    created sign up page

--- loginandsignuppage.cpp
+++ loginandsignuppage.cpp
@@ -6,7 +6,24 @@ vector <string> username;
 vector<string> passwords;
 int main()
 {
-
+    string name, uname, p1, p2;
+    cout<<"Enter Your Name: ";
+    getline(cin,name);
+    cout<<endl<<"Create a user name: ";
+    getline(cin,uname);
+    username.push_back(uname);
+    int n = username.size();
+    for(int i=0; i<=n-2; i++){
+        if(username[i] == username[n-1]) cout<<"Please enter another username"<<endl;
+    }
+    cout<<endl<<"Enter password: ";
+    getline(cin,p1);
+    cout<<endl<<"Re-enter the password: ";
+    getline(cin,p2);
+    if(p1 != p2) cout<<"Please recheck the password: ";
+    else passwords.push_back(p1);
+    cout<<endl<<"Submit";   
+}
 
 
 
