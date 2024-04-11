eef0d5b9a8a798fbec71abce233823b74b561b3a
geekyparth2004 <154241652+geekyparth2004@users.noreply.github.com>
2024-04-11 08:37:20 +0530

    Update SighInPage.cpp

--- SighInPage.cpp
+++ SighInPage.cpp
@@ -5,6 +5,24 @@ using namespace std;
 vector <string> username;
 vector<string> passwords;
 
+void SignUp(string name, string uname, string p1, string p2){
+    cout<<"Enter Your Name: ";
+    getline(cin,name);
+    cout<<endl<<"Create a user name: ";
+    getline(cin,uname);
+    username.push_back(uname);
+    int n = username.size();
+    for(int i=0; i<=n-2; i++){
+        if(username[i] == username[n-1]) cout<<"Please enter another username"<<endl;
+    }
+    cout<<endl<<"Enter password: ";
+    getline(cin,p1);
+    cout<<endl<<"Re-enter the password: ";
+    getline(cin,p2);
+    if(p1 != p2) cout<<"Please recheck the password: ";
+    else passwords.push_back(p1);
+    cout<<endl<<"Submit";   
+}
 
 void SignIn(string uname, string pass){
     cout<<"Enter your username: ";
@@ -27,6 +45,18 @@ void SignIn(string uname, string pass){
     }
 }
 int main(){
+    int n; 
+    cout<<"What you want to do ?"; 
+    cout<<"Press 1 for SignUp and Press 2 for Login; 
+    cin>>n; 
+
+    if(n==1)
+    {
+        
+    }
+        
+    
     string name, uname, pass, re_pass;
-    SignIn(uname,pass);
+    SignIn(uname,pass)
+    
 }
